8f9c1c7df48397b56c243335ed5276a7bb4bcf3b
Rahul Sinha <rahul.sinha1908@gmail.com>
2018-03-09 14:02:57 +0530

    Made the structure

--- app.js
+++ app.js
@@ -5,9 +5,6 @@ var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
-var index = require('./routes/index');
-var users = require('./routes/users');
-
 var app = express();
 
 // view engine setup
@@ -22,8 +19,10 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', index);
-app.use('/users', users);
+app.use('/', require('./routes/users/user-index'));
+app.use('/faculty', require('./routes/faculty/faculty-index'));
+app.use('/hod', require('./routes/hod/hod-index'));
+app.use('/admin', require('./routes/admin/admin-index'));
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
