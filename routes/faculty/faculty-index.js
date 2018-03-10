a6b223ad66471c019b0236ef5cecf213847c6a0f
naman0105 <naman0105@gmail.com>
2018-03-10 19:10:21 +0530

    added login form, faculty dashboard

--- routes/faculty/faculty-index.js
+++ routes/faculty/faculty-index.js
@@ -3,8 +3,23 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  console.log("you just sent " + req.body.name);
+  res.render('faculty/index', { title: 'Express' });
 });
+
+router.get('/login', function(req, res, next) {
+  res.render('faculty/login', { title: 'Express' });
+});
+
+router.get('/personnal-info', function(req, res, next) {
+  // document.getElementById('personnallist').className = "active";
+  res.render('faculty/personnal-info', { title: 'Express' });
+});
+
+router.get('/service-details', function(req, res, next) {
+  res.render('faculty/service-details', { title: 'Express' });
+});
+
 
 router.use('/init', require('./f-init'))
 
