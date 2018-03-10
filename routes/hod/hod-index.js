578a05792e7740afc601990010af22be4c816222
Puneethnaik <puneethn250@gmail.com>
2018-03-10 23:00:24 +0530

    added hod portfolio

--- routes/hod/hod-index.js
+++ routes/hod/hod-index.js
@@ -3,7 +3,22 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('hod/index', { title: 'Express' });
 });
 
+router.get('/login', function(req, res, next) {
+  res.render('faculty/login', { title: 'Express' });
+});
+
+router.get('/personnal-info', function(req, res, next) {
+  // document.getElementById('personnallist').className = "active";
+  res.render('faculty/personnal-info', { title: 'Express' });
+});
+
+router.get('/service-details', function(req, res, next) {
+  res.render('faculty/service-details', { title: 'Express' });
+});
+
+
+router.use('/init', require('./hod-init'))
 module.exports = router;
