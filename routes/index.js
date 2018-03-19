4b26de393fd63eb27bc1c88755b53fa44004639e
naman0105 <naman0105@gmail.com>
2018-03-19 23:09:30 +0530

    tried to integrate extractExcel

--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,15 @@
 var express = require('express');
 var router = express.Router();
-
+var extractExcel = require('../back-end/excelExtract');
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+router.get('/excelExtract', function(req,res,next){
+  var path = './back-end/excelSheets/service.xlsx';
+  extractExcel(path);
+  res.render('index2');
+})
 
 module.exports = router;
