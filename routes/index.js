a2ce179004f1eb47d976c975744ce388f3c2fd9c
Puneethnaik <puneethn250@gmail.com>
2018-03-22 08:42:53 +0530

    added RESTful API

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,9 @@
 var express = require('express');
 var router = express.Router();
 var extractExcel = require('../back-end/excelExtract');
+var sqlAPI = require('../back-end/sqlAPI');
+var sqlExecute = require('./apis/mySqlCalls');
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
@@ -10,6 +13,27 @@ router.get('/excelExtract', function(req,res,next){
   var path = './back-end/excelSheets/service.xlsx';
   extractExcel(path);
   res.render('index2');
-})
+});
+
+//RESTful API to get information
+router.get('/data/:tableName/:columnName', function(req, res, next){
+    callback = function(result){
+        res.setHeader('Content-Type', 'application/json');
+        res.send(result);
+        //res.send(sqlAPI.fetchResults(req.params.columnName, req.params.tableName));
+    }
+    sqlAPI.fetchResults(req.params.columnName, req.params.tableName, '', callback);
+});
+
+router.get('/data/:tableName/', function(req, res, next){
+    var callback = function(err, result){
+      if(err)
+        throw err;
+      console.log(result);
+      res.setHeader('Content-Type', 'application/json');
+      res.send(result);
+    }
+    sqlExecute.getWholeTable(callback, req.params.tableName);
+});
 
 module.exports = router;
