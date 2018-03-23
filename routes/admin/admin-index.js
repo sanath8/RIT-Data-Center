32c0ebb04bfcfdd203212532a505fd539c25f543
sanath <sanathbhargav26@gmail.com>
2018-03-23 17:32:32 +0530

    excel extract date bug fixed

--- routes/admin/admin-index.js
+++ routes/admin/admin-index.js
@@ -3,7 +3,14 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  console.log("you just sent " + req.body.name);
+  res.render('admin/index', { title: 'Express', type:"dashboard", });
 });
+
+router.get('/login', function(req, res, next) {
+  res.render('admin/login', { title: 'Express', type:"login" });
+});
+router.use('/init', require('./a-init'))
+
 
 module.exports = router;
