cfaa2c9e7dc7a9fdc07f66034ef0463705b05a82
Rahul Sinha <rahul.sinha1908@gmail.com>
2018-03-24 12:27:19 +0530

    made changes in data end-point apis.

---
+++ routes/apis/dataApi.js
@@ -0,0 +1,26 @@
+var express = require('express');
+var router = express.Router();
+
+router.post('/:tableName/', function(req, res, next){
+    callback = function(result){
+        res.setHeader('Content-Type', 'application/json');
+        res.send(result);
+        //res.send(sqlAPI.fetchResults(req.params.columnName, req.params.tableName));
+    }
+    console.log("body recieved" + JSON.stringify(req.body) + " " + req.body.schema);
+    sqlAPI.fetchResults(req.body.schema, req.params.tableName, Array(req.body.whereOption), callback);
+});
+
+router.get('/:tableName/', function(req, res, next){
+    var callback = function(err, result){
+      if(err)
+        throw err;
+      console.log(result);
+      res.setHeader('Content-Type', 'application/json');
+      res.send(result);
+    }
+    sqlExecute.getWholeTable(callback, req.params.tableName);
+});
+
+
+module.exports = router;
