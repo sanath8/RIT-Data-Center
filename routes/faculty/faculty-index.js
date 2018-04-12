a9d96526853732eddaca072f1988f7c361627d43
naman0105 <naman0105@gmail.com>
2018-04-12 21:34:09 +0530

    excel generator working

--- routes/faculty/faculty-index.js
+++ routes/faculty/faculty-index.js
@@ -86,12 +86,13 @@ router.get('/generateexcelTest/:jsonObject',function(req,res,next){
   //res.redirect('/faculty/reports');
 });
 
-router.get('/generateexcel/:tableNo/:index',function(req,res,next){
+router.get('/generateexcel/:tableNo/:index/:facultyId',function(req,res,next){
   if(!utility.checkSesssion(req, res)) return;
   console.log("this is " + req.params.facultyTable);
   var map=["", "", "", "", "", "",""];
   var index = req.params.index;
-  var fid = req.session.facultyId;
+  var fid = req.params.facultyId;
+
   var tableno = parseInt(req.params.tableNo)-1;
   var callback=function(err, result){
 	if(index == 1 || index == 2 || index == 3){
