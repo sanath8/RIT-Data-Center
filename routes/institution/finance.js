1760e64025847c110859f4395784ee6e562ea0f3
sammedravanavar <sammedravanavar@gmail.com>
2018-04-28 12:57:13 +0530

    institution sql calls

---
+++ routes/institution/finance.js
@@ -0,0 +1,44 @@
+var express = require('express');
+var router = express.Router();
+var sqlExecute = require('../apis/mySqlCalls');
+var utility = require('../utilities');
+
+router.get('/', function(req, res, next) {
+	if(!utility.checkSesssion(req, res)) return;
+
+	//var departmentId;
+	var auth = true;
+
+	// if(!utility.checkGetParam(req,res)){
+	// 	departmentId = req.session.departmentId;
+	// }
+	// else{
+	// 	auth = false;
+	// 	departmentId = req.query.fId;
+	// }
+	// to remove when department session is used
+	departmentId = "cse";
+
+	var callback = function(err, data){
+		if(err)
+			throw err;
+		//var departmentID = req.session.departmentId;
+		//console.log("GetParam : " + req.session.departmentId);
+		//console.log("department id : " + departmentID);
+		//console.log(JSON.stringify(data));
+		var data = {
+			finance_committee: data
+		}
+		res.render('institution/finance', {title : "Finance Committee", type:"finance", data:data,
+        
+        GetParam: req.query.deptId,
+
+        authType:req.session.departmentId
+        });
+		
+	}
+	sqlExecute.getFinanceCommittee(callback);
+});
+
+
+module.exports=router;
