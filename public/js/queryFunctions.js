f7eecbcbdf54076c9f88ed73faa9ffb07e5d94e4
sanath <sanathbhargav26@gmail.com>
2018-06-11 00:37:53 +0530

    bugs in filter page fixed

--- public/js/queryFunctions.js
+++ public/js/queryFunctions.js
@@ -1,6 +1,6 @@
 var columnsSelected = "";
 var tableLock = 0;
-var facultyLevelTables =["book","book_chapter","conference_paper","consultancy",
+var facultyLevelTables =["faculty", "book","book_chapter","conference_paper","consultancy",
 "courses_handled","faculty_conference_symposia","faculty_guest_lecture","faculty_patent",
 "faculty_qualification","faculty_research","faculty_service","faculty_workshop_fdp","funded_projects",
 "industrial_collaboraion_mou","journal_paper","phd_scholar","projects_handled"]; //tables that contain faculty-ID
