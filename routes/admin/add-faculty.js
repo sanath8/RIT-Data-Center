ba1963d29268cb6f1be2819663340ed823586ed7
Puneethnaik <puneethn250@gmail.com>
2018-08-28 08:42:40 +0530

    admin access logic

--- routes/admin/add-faculty.js
+++ routes/admin/add-faculty.js
@@ -5,7 +5,10 @@ var utility = require('../utilities');
 
 router.get('/', function(req, res, next) {
     utility.checkSesssion(req, res);
-
+    if(facultyId !== 'admin'){
+		res.redirect("/error/401");
+		return;
+	}
     res.render('admin/add-faculty', {type : 'add-faculty', GetParam: req.query.fId, authType:req.session.facultyId, departmentId:req.session.departmentId, GetParam:"dummy" });
 })
 
