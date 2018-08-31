6b69416091d5614ea28102d9fdb4f84e41cdccbe
sammedravanavar <sammedravanavar@gmail.com>
2018-08-31 15:55:18 +0530

    dropdowns added

---
+++ public/dropdowns/dropdown.js
@@ -0,0 +1,25 @@
+var dropdowns = {
+    "faculty":{
+        "natureOfAppointment":["Regular","Visiting"],
+        "designation":["Professor","Associate Professor","Assistant Professor"],
+        "gender":["Male","Female"]
+    },
+    "faculty_workshop_fdp":{
+        "type":["National","International"]
+    },
+    "faculty_conference_symposia":{
+        "invitedOrDeputed":["Invited","Deputed"]
+    },
+    "conference_paper":{
+        "conferenceType":["National","International"]
+    },
+    "journal_paper":{
+        "journalType":["National","International"]
+    },
+    "courses_handled":{
+        "ugOrPg":["Ug","Pg"]
+    },
+    "projects_handled":{
+        "ugOrPg":["Ug","Pg"]
+    }
+}
