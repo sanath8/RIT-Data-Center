3bb83ce59b1b760b890d6036d4795ddfc206abd0
sanath <sanathbhargav26@gmail.com>
2018-09-04 18:13:22 +0530

    publication filter added

---
+++ public/js/dataSets.js
@@ -0,0 +1,8 @@
+var facultyLevelTables =["faculty", "book","book_chapter","conference_paper","consultancy",
+"courses_handled","faculty_conference_symposia","faculty_guest_lecture","faculty_patent",
+"faculty_qualification","faculty_research","faculty_service","faculty_workshop_fdp","funded_projects",
+"industrial_collaboraion_mou","journal_paper","phd_scholar","projects_handled"]; //tables that contain faculty-ID
+
+
+
+var enablePublicTypeTable = ["conference_paper", "journal_paper"];
