4f5796d218c642d96dbcf6f527971a3e2a3f88de
sammedravanavar <sammedravanavar@gmail.com>
2018-09-27 19:17:54 +0530

    add faculty bugs fixed

--- routes/apis/new-faculty.js
+++ routes/apis/new-faculty.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var router = express.Router();
 var mysql = require('./mySqlCalls');
+var mailer = require("./mailer");
 
 router.post('/', function(req, res, next){
     var facultyId = req.body.department;
@@ -19,14 +20,19 @@ router.post('/', function(req, res, next){
                 count = 0 + count.toString();
             }
             facultyId  = facultyId + count;
+            var passChar = "abcdefghijklmnopqrstuvwxyz0123456789";
+            var password = "";
+            for(var i=0;i<6;i++) password += passChar.charAt(Math.floor(Math.random()*passChar.length));
             sqlQuery = "INSERT INTO faculty VALUES (\"" + facultyId+"\",\""+req.body.name+"\",\""+req.body.gender+"\",\""+"null"+"\",\""+"null"+"\",\""
-            +"null"+"\",\""+"null"+"\",\""+"null"+"\",\""+req.body.natureOfAppointment+"\",\""+req.body.contactNumber+"\",\""+req.body.email+"\",\""+facultyId+"\",\""
-            +"null"+"\",\""+"null"+"\",\""+"null"+"\",\""+"null"+"\",\""+req.body.designation+"\",\""+req.body.department+"\")"            
+            +"null"+"\",\""+"null"+"\",\""+"null"+"\",\""+req.body.natureOfAppointment+"\",\""+req.body.contactNumber+"\",\""+req.body.email+"\",\""+password+"\",\""
+            +"null"+"\",\""+"null"+"\",\""+"null"+"\",\""+"null"+"\",\""+req.body.designation+"\",\""+"null"+"\",\""+"null"+"\",\""+"null"+"\",\""+"null"+"\",\""
+            +"null"+"\",\""+"null"+"\",\""+"null"+"\",\""+"null"+"\",\""+req.body.department+"\")"            
             mysql.runRawQuery(sqlQuery, function(err, result){
                 if(err){
                     res.end("Error : "+err.message);
                     return;
                 } else{
+                    mailer.sendMail(req.body.email, password);
                     res.redirect('back')
                 }
             })
