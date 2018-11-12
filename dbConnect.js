1e4df8917da38951778546e91c58f53ab580bbfe
naman0105 <naman0105@gmail.com>
2018-11-12 22:55:44 +0530

    changed the hardwaer

--- dbConnect.js
+++ dbConnect.js
@@ -6,8 +6,7 @@ exports.connectDB = function ()
 	var con = mysql.createConnection
 	(
 		{
-			host: "localhost",
-			//host: "172.1.16.110",
+			host: "172.1.16.110",
 			user: "root",
 			password: "mysql12345",
 			database: "rit_data_center"
