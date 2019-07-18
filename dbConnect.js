145ba23cfa6022803a1e91d7a5e0c032ce895664
Anshulg3237 <anshulg3237@gmail.com>
2019-07-18 12:27:51 +0530

    Add files via upload

--- dbConnect.js
+++ dbConnect.js
@@ -6,10 +6,12 @@ exports.connectDB = function ()
 	var con = mysql.createConnection
 	(
 		{
-			host: "172.1.16.110",
+			// host: "172.1.16.110",
+      host: "localhost",
 			user: "root",
-			password: "mysql12345",
-			database: "rit_data_center"
+			// password: "mysql12345",
+      password:"",
+			database: "rit_data_center_new"
 		}
 	);
 	con.connect
